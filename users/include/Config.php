eeaedc99c3977550615b737dec43795b4a2e09b3
Kevin Barasa <kevin.barasa001@gmail.com>
2016-12-14 19:02:03 +0300

    REST implemented. Protected with index.php

---
+++ users/include/Config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "amn_kev");
+define("DB_PASSWORD", "Kevcom2015");
+define("DB_DATABASE", "amn_arosto");
+?>
