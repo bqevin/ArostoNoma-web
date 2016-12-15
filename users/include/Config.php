f88bd5c077be83bb44baf878edf42a17ac3d629c
Kevin Barasa <kevin.barasa001@gmail.com>
2016-12-15 21:25:54 +0300

    Semantic UI frontend

--- users/include/Config.php
+++ users/include/Config.php
@@ -4,7 +4,7 @@
  * Database config variables
  */
 define("DB_HOST", "localhost");
-define("DB_USER", "amn_kev");
+define("DB_USER", "amn_arosto");
 define("DB_PASSWORD", "Kevcom2015");
 define("DB_DATABASE", "amn_arosto");
 ?>
